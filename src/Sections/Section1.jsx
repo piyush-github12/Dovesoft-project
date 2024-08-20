f19890b354e4d064b2527ae857349febdd9b0393
piyush-github12 <piyushgithubaccouunt@gmail.com>
2024-08-20 16:02:30 +0530

    fix

--- src/Sections/Section1.jsx
+++ src/Sections/Section1.jsx
@@ -40,7 +40,7 @@ const Section1 = () => {
         stagger: 2,
         keyframes: [
           { y: 0, duration: 1, delay: 1 },
-          { y: "-110%", duration: 1, delay: 1 },
+          { y: "-250px", duration: 1, delay: 1 },
         ],
         repeat: -1,
       });
@@ -60,64 +60,74 @@ const Section1 = () => {
     <div className=" w-screen h-screen flex relative overflow-hidden bg-gradient-to-r from-[#cad4ff] to-[#95BFFF]">
       <div
         id="maincircle"
-        className="maincircle w-[40vw] h-[40vw] max-lg:w-[60vw] max-lg:h-[60vw] max-md:min-h-[600px] max-md:min-w-[600px]  rotate-[-45deg] absolute right-[-12vw] top-[-12vw] max-lg:right-[-20vw] max-lg:top-[-20vw] max-md:right-[-300px] max-md:top-[-300px]  border-[30px]  border-[#ff8a0496] rounded-full flex items-center justify-center text-white"
+        className="maincircle w-[40vw] h-[40vw] max-lg:w-[60vw] max-lg:h-[60vw] max-md:min-h-[500px] max-md:min-w-[500px]  rotate-[-45deg] absolute right-[-12vw] top-[-12vw] max-lg:right-[-20vw] max-lg:top-[-20vw] max-md:right-[-200px] max-md:top-[-200px]  border-[30px]  border-[#ff8a0496] rounded-full flex items-center justify-center text-white"
       >
         <div className="line w-full h-[10px]  rotate-[0deg] absolute">
           <div className="smallcircle w-[170px] h-[170px] rotate-[45deg] border-[0px] border-[#B9D6FF] bg-[#ff4747] rounded-full absolute translate-x-[-50%] translate-y-[-50%] flex flex-col items-center justify-center">
             <BiMessageDetail className="text-[40px] " />
-            <h1 className="font-extrabold text-[2vw]">SMS</h1>
+            <h1 className="font-extrabold text-[2vw] max-md:text-[20px]">
+              SMS
+            </h1>
           </div>
           <div className="smallcirclefirst z-[-1] w-[170px] h-[170px] rotate-[45deg] border-[0px] border-[#B9D6FF] bg-[#B9D6FF] rounded-full absolute translate-x-[-50%] translate-y-[-50%] flex flex-col items-center justify-center"></div>
         </div>
         <div className="line w-full h-[10px]  rotate-[72deg]  absolute ">
           <div className="smallcircle w-[170px] h-[170px] rotate-[45deg] border-[0px] border-[#B9D6FF] bg-[#47cbff] rounded-full absolute translate-x-[-50%] translate-y-[-50%] flex flex-col items-center justify-center">
             <RiUserVoiceLine className="text-[40px]" />
-            <h1 className="font-extrabold text-[2vw]">Voice</h1>
+            <h1 className="font-extrabold text-[2vw ] max-md:text-[20px]">
+              Voice
+            </h1>
           </div>
           <div className="smallcirclefirst z-[-1] w-[170px] h-[170px] rotate-[45deg] border-[0px] border-[#B9D6FF] bg-[#B9D6FF] rounded-full absolute translate-x-[-50%] translate-y-[-50%] flex flex-col items-center justify-center"></div>
         </div>
         <div className="line w-full h-[10px]  rotate-[144deg] absolute">
           <div className="smallcircle w-[170px] h-[170px] rotate-[45deg] border-[0px] border-[#B9D6FF] bg-[#ff8147] rounded-full absolute translate-x-[-50%] translate-y-[-50%] flex flex-col items-center justify-center">
             <MdOutlineMail className="text-[40px]" />
-            <h1 className="font-extrabold text-[2vw]">Email</h1>
+            <h1 className="font-extrabold text-[2vw ] max-md:text-[20px]">
+              Email
+            </h1>
           </div>
           <div className="smallcirclefirst z-[-1] w-[170px] h-[170px] rotate-[45deg] border-[0px] border-[#B9D6FF] bg-[#B9D6FF] rounded-full absolute translate-x-[-50%] translate-y-[-50%] flex flex-col items-center justify-center"></div>
         </div>
         <div className="line w-full h-[10px]  rotate-[216deg] absolute">
           <div className="smallcircle w-[170px] h-[170px] rotate-[45deg] border-[0px] border-[#B9D6FF] bg-[#29ae5e] rounded-full absolute translate-x-[-50%] translate-y-[-50%] flex flex-col items-center justify-center">
             <FaWhatsapp className="text-[40px]" />
-            <h1 className="font-extrabold text-[1.8vw]">Whatsapp</h1>
+            <h1 className="font-extrabold text-[1.8vw] max-md:text-[20px]">
+              Whatsapp
+            </h1>
           </div>
           <div className="smallcirclefirst z-[-1] w-[170px] h-[170px] rotate-[45deg] border-[0px] border-[#B9D6FF] bg-[#B9D6FF] rounded-full absolute translate-x-[-50%] translate-y-[-50%] flex flex-col items-center justify-center"></div>
         </div>
         <div className="line w-full h-[10px]  rotate-[288deg] absolute">
           <div className="smallcircle w-[170px] h-[170px] rotate-[45deg] border-[0px] border-[#B9D6FF] bg-[#4781ff] rounded-full absolute translate-x-[-50%] translate-y-[-50%] flex flex-col items-center justify-center">
             <MdOutlineMessage className="text-[40px]" />
-            <h1 className="font-extrabold text-[2vw]">RCS</h1>
+            <h1 className="font-extrabold text-[2vw ] max-md:text-[20px]">
+              RCS
+            </h1>
           </div>
           <div className="smallcirclefirst z-[-1] w-[170px] h-[170px] rotate-[45deg] border-[0px] border-[#B9D6FF] bg-[#B9D6FF] rounded-full absolute translate-x-[-50%] translate-y-[-50%] flex flex-col items-center justify-center"></div>
         </div>
       </div>
       <div className="w-[60vw] h-[60vw] absolute bottom-0 left-0 translate-x-[-30%] translate-y-[40%] bg-[#92AAFF] blur-[100px]  rounded-full "></div>
       <div className="w-[50%] h-screen pl-[6vw]  z-10 flex flex-col justify-center   gap-5">
-        <div className="relative w-full h-[15vw]  max-md:min-h-[120px] max-md:mt-[100px] overflow-hidden ">
-          <h1 className="heading1  text-[3.5vw] max-md:text-[5vw] max-md:leading-[5vw] h-fit font-extrabold leading-[4vw] absolute ">
+        <div className="relative w-full h-[200px]  max-md:h-[120px] max-md:mt-[200px] overflow-hidden ">
+          <h1 className="heading1 h-[200px] max-md:h-[120px]  text-[3.5vw] max-md:text-[5vw] max-md:leading-[5vw]  font-extrabold leading-[4vw] absolute ">
             Effortlessly Simplified & <br /> Powerful Multi-Channel <br />
             CPaaS Platform.
           </h1>
-          <h1 className="heading1  text-[3.5vw] max-md:text-[5vw] max-md:leading-[5vw] h-fit font-bold leading-[4vw] absolute translate-y-[110%]">
+          <h1 className="heading1 h-[200px] max-md:h-[120px]  text-[3.5vw] max-md:text-[5vw] max-md:leading-[5vw]  font-bold leading-[4vw] absolute translate-y-[250px]">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque,
             vitae!
           </h1>
-          <h1 className="heading1  text-[3.5vw] max-md:text-[5vw] max-md:leading-[5vw] h-fit font-bold leading-[4vw] absolute translate-y-[110%]">
+          <h1 className="heading1 h-[200px] max-md:h-[120px]  text-[3.5vw] max-md:text-[5vw] max-md:leading-[5vw]  font-bold leading-[4vw] absolute translate-y-[250px]">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque,
             vitae!
           </h1>
-          <h1 className="heading1  text-[3.5vw] max-md:text-[5vw] max-md:leading-[5vw] h-fit font-bold leading-[4vw] absolute translate-y-[110%]">
+          <h1 className="heading1 h-[200px] max-md:h-[120px]  text-[3.5vw] max-md:text-[5vw] max-md:leading-[5vw]  font-bold leading-[4vw] absolute translate-y-[250px]">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque,
             vitae!
           </h1>
-          <h1 className="heading1  text-[3.5vw] max-md:text-[5vw] max-md:leading-[5vw] h-fit font-bold leading-[4vw] absolute translate-y-[110%]">
+          <h1 className="heading1 h-[200px] max-md:h-[120px]  text-[3.5vw] max-md:text-[5vw] max-md:leading-[5vw]  font-bold leading-[4vw] absolute translate-y-[250px]">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque,
             vitae!
           </h1>
