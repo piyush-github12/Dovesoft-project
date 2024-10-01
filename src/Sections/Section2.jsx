4ea139deb546c675c0615b2826b60b79cd0536a2
piyush-github12 <piyushgithubaccouunt@gmail.com>
2024-10-01 21:54:50 +0530

    logos images

--- src/Sections/Section2.jsx
+++ src/Sections/Section2.jsx
@@ -7,30 +7,56 @@ const Section2 = () => {
     <div
       id='mq' className="w-fit h-[80px]   bg-white max-sm:h-[100px] flex items-center justify-center overflow-x-auto overflow-y-hidden "
     >
-      <div id="marqutag1" className="marqutag1 w-[1000px] px-5 flex justify-around gap-10  ">
-        <img className="h-[40px]" src="./images/marque1.png" alt="" />
-        <img className="h-[40px]" src="./images/marque2.png" alt="" />
-        <img className="h-[40px]" src="./images/marque3.png" alt="" />
-        <img className="h-[40px]" src="./images/marque4.png" alt="" />
-        <img className="h-[40px]" src="./images/marque5.png" alt="" />
-        <img className="h-[40px]" src="./images/marque6.png" alt="" />
+      <div id="marqutag1" className="marqutag1 w-[3080px] px-[20px] flex justify-around gap-10  ">
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/1.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/2.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/3.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/4.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/5.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/6.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/7.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/8.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/9.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/10.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/11.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/12.jpg" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/13.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/14.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/15.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/16.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/17.jpg" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/18.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/19.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/20.svg" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/21.webp" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/22.webp" alt="" />
       </div>
-      <div id="marqutag1" className="marqutag1 w-[1000px] px-5 flex justify-around gap-10 ">
-        <img className="h-[40px]" src="./images/marque1.png" alt="" />
-        <img className="h-[40px]" src="./images/marque2.png" alt="" />
-        <img className="h-[40px]" src="./images/marque3.png" alt="" />
-        <img className="h-[40px]" src="./images/marque4.png" alt="" />
-        <img className="h-[40px]" src="./images/marque5.png" alt="" />
-        <img className="h-[40px]" src="./images/marque6.png" alt="" />
+      <div id="marqutag1" className="marqutag1 w-[3080px] px-[20px] flex justify-around gap-10  ">
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/1.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/2.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/3.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/4.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/5.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/6.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/7.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/8.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/9.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/10.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/11.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/12.jpg" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/13.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/14.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/15.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/16.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/17.jpg" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/18.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/19.png" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/20.svg" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/21.webp" alt="" />
+        <img className="h-[50px] w-[100px] object-contain grayscale" src="./images/marquelogo/22.webp" alt="" />
       </div>
-      <div id="marqutag1" className="marqutag1 w-[1000px] px-5 flex justify-around gap-10 ">
-        <img className="h-[40px]" src="./images/marque1.png" alt="" />
-        <img className="h-[40px]" src="./images/marque2.png" alt="" />
-        <img className="h-[40px]" src="./images/marque3.png" alt="" />
-        <img className="h-[40px]" src="./images/marque4.png" alt="" />
-        <img className="h-[40px]" src="./images/marque5.png" alt="" />
-        <img className="h-[40px]" src="./images/marque6.png" alt="" />
-      </div>
+     
+     
     </div>
   );
 }
